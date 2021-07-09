9fbc4206bb0da9ea122cef42b1618f2b2ba290e9
Festus obi <fessy_wonder@yahoo.co.uk>
2021-07-09 14:15:58 +0200

    Added inMemoryRepository

---
+++ Repositories/InMemItemsRepository.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Catalog.Models;
+using System.Linq;
+
+namespace Catalog.Repositories
+{
+    public class InMemItemsRepository
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, DateCreated = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 9, DateCreated = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 9, DateCreated = DateTimeOffset.UtcNow }
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+        }
+    }
+}
