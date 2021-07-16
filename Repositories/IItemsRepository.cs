3db854aa9dbab5287850c4e948703e40eca1d412
Festus obi <fessy_wonder@yahoo.co.uk>
2021-07-16 18:33:32 +0200

    made actions & calls async

--- Repositories/IItemsRepository.cs
+++ Repositories/IItemsRepository.cs
@@ -1,15 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Catalog.Models;
 
 namespace Catalog.Repositories
 {
     public interface IItemsRepository
     {
-        Item GetItem(Guid id);
-        IEnumerable<Item> GetItems();
-        void DeleteItem(Guid id);
-        void UpdateItem(Item item);
-        void CreateItem(Item item);
+        Task<Item> GetItemAsync(Guid id);
+        Task<IEnumerable<Item>> GetItemsAsync();
+        Task DeleteItemAsync(Guid id);
+        Task UpdateItemAsync(Item item);
+        Task CreateItemAsync(Item item);
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,10 @@ namespace Catalog
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddControllers(options =>
+            {
+                options.SuppressAsyncSuffixInActionNames = false;
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog", Version = "v1" });
