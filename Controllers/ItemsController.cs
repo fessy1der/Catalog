f1a2c4b3a86d84ead270674f70d9827aa81dc8ab
Festus obi <fessy_wonder@yahoo.co.uk>
2021-07-21 22:40:37 +0200

    Added logs using Ilogger

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -6,6 +6,7 @@ using Catalog.Dtos;
 using Catalog.Models;
 using Catalog.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Catalog.Controllers
 {
@@ -14,16 +15,19 @@ namespace Catalog.Controllers
     public class ItemsController : ControllerBase
     {
         private readonly IItemsRepository _repository;
+        private readonly ILogger<ItemsController> _logger;
 
-        public ItemsController(IItemsRepository repository)
+        public ItemsController(IItemsRepository repository, ILogger<ItemsController> logger)
         {
             _repository = repository;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<IEnumerable<ItemDto>> GetItemsAsync()
         {
             var items = (await _repository.GetItemsAsync()).Select(item => item.AsDto());
+            _logger.LogInformation($"{DateTime.UtcNow.ToString("hh:mm:ss")}: Retrived {items.Count()} items");
             return items;
         }
 
