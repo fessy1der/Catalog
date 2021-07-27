05fc6c7d6cfae5afffffa02667d87969b56f91a9
Festus obi <fessy_wonder@yahoo.co.uk>
2021-07-27 08:11:22 +0200

    Added GetItems test

--- Catalog.Tests/ItemsControllerTests.cs
+++ Catalog.Tests/ItemsControllerTests.cs
@@ -2,17 +2,66 @@ using System;
 using Xunit;
 using Catalog.Api.Repositories;
 using Catalog.Api.Models;
+using Catalog.Api.Controllers;
+using Catalog.Api.Dtos;
 using Moq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Catalog.Tests
 {
     public class ItemsControllerTests
     {
+        private readonly Mock<IItemsRepository> repositoryStub = new();
+        private readonly Mock<ILogger<ItemsController>> loggerStub = new();
+        private readonly Random rand = new();
+
         [Fact]
-        public void GetItemAsync_WithUnexistingItem_ReturnNotFound()
+        public async Task GetItemAsync_WithUnexistingItem_ReturnNotFound()
         {
-            var repositoryStub = new Mock<IItemsRepository>();
+            //Arrange
             repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>())).ReturnsAsync((Item)null);
+
+            var controller = new ItemsController(repositoryStub.Object, loggerstub.Object);
+
+            //Act
+            var result = await controller.GetItemAsync(Guid.NewGuid());
+
+            //Assert
+            Assert.IsType<NotFoundResult>(result.Result);
         }
+
+        [Fact]
+        public async Task GetItemAsync_WithExistingItem_ReturnExpectedItem()
+        {
+            //Arrange
+            var expectedItem = CreateRandomItem();
+
+            repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>())).ReturnsAsync(expectedItem);
+
+            var controller = new ItemsController(repositoryStub.Object, loggerstub.Object);
+
+            //Act
+            var result = await controller.GetItemAsync(Guid.NewGuid());
+
+            //Assert
+            Assert.IsType<ItemDto>(result.Value);
+            var dto = (result as ActionResult<ItemDto>).Value;
+            Assert.Equal(expectedItem.Id, dto.Id);
+            Assert.Equal(expectedItem.Name, dto.Name);
+        }
+
+        private Item CreateRandomItem()
+        {
+            return new()
+            {
+                Id = Guid.NewGuid(),
+                Name = Guid.NewGuid().ToString(),
+                Price = rand.Next(1000),
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+        }
+
     }
 }
